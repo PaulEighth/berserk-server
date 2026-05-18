a90050fb838ad41d7a68fe7af8efc767f2dc0ba9
PaulEighth <original774lv@gmail.com>
2026-05-18 17:29:49 +0300

    Fix postgres SSL and tournament tracking

--- db.js
+++ db.js
@@ -7,9 +7,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || "5432", 10),
-ssl: {
-  rejectUnauthorized: false
-}
+ssl: false
 });
 
 module.exports = pool;
